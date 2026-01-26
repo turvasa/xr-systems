5cb8c02d3be9018089fac35d8b6928fe94f0a66a
turvasa <ttallava19@student.oulu.fi>
2026-01-26 17:05:33 +0200

    The Room; ready

--- unity/Assets/Quit.cs
+++ unity/Assets/Quit.cs
@@ -1,19 +1,18 @@
+using UnityEngine.InputSystem;
 using UnityEngine;
 
-public class Quit
-{
+public class Quit : MonoBehaviour {
     public InputActionReference action;
 
-    void Start()
-    {
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
         action.action.Enable();
-        action.action.performed += (ctx) =>
-        {
+        action.action.performed += (ctx) => {
             #if UNITY_EDITOR
                 UnityEditor.EditorApplication.isPlaying = false;
             #else
                 Application.Quit();
             #endif
-        }
+        };
     }
 }
